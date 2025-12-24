b835d7126fd68a8936c9f04590991192d286ad16
AuroraLantean <raymondlieu.crypto@runbox.com>
2025-12-24 11:17:37 +0200

    fix indentation in tok22InitATA

--- src/instructions/tok22InitATA.rs
+++ src/instructions/tok22InitATA.rs
@@ -3,94 +3,94 @@ use pinocchio::{account_info::AccountInfo, program_error::ProgramError, ProgramR
 use pinocchio_log::log;
 
 use crate::{
-    check_mint22a, empty_data, empty_lamport, executable, instructions::check_signer, rent_exempt,
-    writable,
+  check_mint22a, empty_data, empty_lamport, executable, instructions::check_signer, rent_exempt,
+  writable,
 };
 
 /// Token2022 Init ATA(Associated Token Account)
 pub struct Token2022InitAta<'a> {
-    pub payer: &'a AccountInfo,
-    pub to_wallet: &'a AccountInfo,
-    pub mint: &'a AccountInfo,
-    pub token_account: &'a AccountInfo,
-    pub token_program: &'a AccountInfo,
-    pub system_program: &'a AccountInfo,
-    pub atoken_program: &'a AccountInfo,
+  pub payer: &'a AccountInfo,
+  pub to_wallet: &'a AccountInfo,
+  pub mint: &'a AccountInfo,
+  pub token_account: &'a AccountInfo,
+  pub token_program: &'a AccountInfo,
+  pub system_program: &'a AccountInfo,
+  pub atoken_program: &'a AccountInfo,
 }
 impl<'a> Token2022InitAta<'a> {
-    pub const DISCRIMINATOR: &'a u8 = &9;
+  pub const DISCRIMINATOR: &'a u8 = &9;
 
-    pub fn process(self) -> ProgramResult {
-        let Token2022InitAta {
-            payer,
-            to_wallet,
-            mint,
-            token_account,
-            token_program,
-            system_program,
-            atoken_program: _,
-        } = self;
-        log!("Token2022InitAta process()");
-        check_signer(payer)?;
-        executable(token_program)?;
+  pub fn process(self) -> ProgramResult {
+    let Token2022InitAta {
+      payer,
+      to_wallet,
+      mint,
+      token_account,
+      token_program,
+      system_program,
+      atoken_program: _,
+    } = self;
+    log!("Token2022InitAta process()");
+    check_signer(payer)?;
+    executable(token_program)?;
 
-        log!("Token2022InitAta 1");
-        rent_exempt(mint, 0)?;
-        check_mint22a(mint, token_program)?;
-        //writable(mint)?;//Shank IDL definition
+    log!("Token2022InitAta 1");
+    rent_exempt(mint, 0)?;
+    check_mint22a(mint, token_program)?;
+    //writable(mint)?;//Shank IDL definition
 
-        log!("Token2022InitAta 2");
-        empty_lamport(token_account)?;
-        empty_data(token_account)?;
-        writable(token_account)?;
+    log!("Token2022InitAta 2");
+    empty_lamport(token_account)?;
+    empty_data(token_account)?;
+    writable(token_account)?;
 
-        log!("Init ATA Token Account");
-        pinocchio_associated_token_account::instructions::Create {
-            funding_account: payer, // Keypair
-            account: token_account,
-            wallet: to_wallet,
-            mint: mint,
-            system_program: system_program,
-            token_program: token_program,
-        }
-        .invoke()?;
-        /*pinocchio_token_2022::instructions::InitializeAccount3 {
-            account: token_account,
-            mint: mint,
-            owner: to_wallet.key(),
-            token_program: token_program.key(),
-        }
-        .invoke()?;//invalid account data for instruction*/
-        Ok(())
+    log!("Init ATA Token Account");
+    pinocchio_associated_token_account::instructions::Create {
+      funding_account: payer, // Keypair
+      account: token_account,
+      wallet: to_wallet,
+      mint: mint,
+      system_program: system_program,
+      token_program: token_program,
     }
-    pub fn init_if_needed(self) -> ProgramResult {
-        match empty_lamport(self.token_account) {
-            Ok(_) => Self::process(self),
-            Err(_) => Ok(()),
-        }
+    .invoke()?;
+    /*pinocchio_token_2022::instructions::InitializeAccount3 {
+        account: token_account,
+        mint: mint,
+        owner: to_wallet.key(),
+        token_program: token_program.key(),
     }
+    .invoke()?;//invalid account data for instruction*/
+    Ok(())
+  }
+  pub fn init_if_needed(self) -> ProgramResult {
+    match empty_lamport(self.token_account) {
+      Ok(_) => Self::process(self),
+      Err(_) => Ok(()),
+    }
+  }
 }
 impl<'a> TryFrom<(&'a [u8], &'a [AccountInfo])> for Token2022InitAta<'a> {
-    type Error = ProgramError;
+  type Error = ProgramError;
 
-    fn try_from(value: (&'a [u8], &'a [AccountInfo])) -> Result<Self, Self::Error> {
-        log!("Token2022InitAta try_from");
-        let (data, accounts) = value;
-        log!("accounts len: {}, data len: {}", accounts.len(), data.len());
+  fn try_from(value: (&'a [u8], &'a [AccountInfo])) -> Result<Self, Self::Error> {
+    log!("Token2022InitAta try_from");
+    let (data, accounts) = value;
+    log!("accounts len: {}, data len: {}", accounts.len(), data.len());
 
-        let [payer, to_wallet, mint, token_account, token_program, system_program, atoken_program] =
-            accounts
-        else {
-            return Err(ProgramError::NotEnoughAccountKeys);
-        };
-        Ok(Self {
-            payer,
-            to_wallet,
-            mint,
-            token_account,
-            token_program,
-            system_program,
-            atoken_program,
-        })
-    }
+    let [payer, to_wallet, mint, token_account, token_program, system_program, atoken_program] =
+      accounts
+    else {
+      return Err(ProgramError::NotEnoughAccountKeys);
+    };
+    Ok(Self {
+      payer,
+      to_wallet,
+      mint,
+      token_account,
+      token_program,
+      system_program,
+      atoken_program,
+    })
+  }
 }
